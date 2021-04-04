71f39b94740a519c87293d3b323deed898ad6793
Innocent WK Chinyemba <innocentwkc@gmail.com>
2021-04-04 10:37:48 +0200

    date update fix

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -2,7 +2,7 @@
   <div
     class="countdown"
     :class="{ 'glitch': glitchActive }"
-    :glitch-text="!switcher ? time  + ' Remaining' : 'Launch Date: 11.12.2020'"
+    :glitch-text="!switcher ? time  + ' Remaining' : 'Launch Date: 10.12.2021'"
   >
     <!-- <span v-if="glitchTextToggle">
         Launch Date: 20.11.2020
@@ -30,10 +30,10 @@ export default {
       glitchActive: false,
       glitchTextToggle: true,
       scrambleText: [
-        'Launch Date: 11.12.2020',
+        'Launch Date: 10.12.2021',
         ''
       ],
-      testText: 'Launch Date: 11.12.2020'
+      testText: 'Launch Date: 10.12.2021'
     }
   },
   computed: {
